aaf0ae9ccd45af703ce34b90e30f023e6ed45266
nreiscaio <nreis.caio@gmail.com>
2022-05-23 21:19:29 -0300

    Aula Senac

---
+++ Mareia Senac/Aula01_23-05-2022/primeiroPrograma.dart
@@ -0,0 +1,9 @@
+//Curso de Tecnologia da Informação
+//Primeiro Programa em Dart: Hello Word!
+
+
+void main() {
+  //Primeiro Programa em Dart
+  //Printando no terminal com Code Runner
+  print('Hello Word!!!');
+}
