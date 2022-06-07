8dd7b0933223db22242b6f6aa496aad92fc86425
nreiscaio <nreis.caio@gmail.com>
2022-06-06 21:39:27 -0300

    Aula Senac

---
+++ Senac/Aula08_06-06-2022/funcaoArrow.dart
@@ -0,0 +1,18 @@
+void main() {
+  //Criar uma função anônima
+  var produto = (int a, int b) {
+    return a * b;
+  }; //Lembrar;
+
+  //Arrow Function
+  //Possui 1 sentença de código
+  var divisao = (double a, double b) => a / b;
+  var modulo = (double c, double d) => c % d;
+  var media = (double n1, double n2, double n3) => (n1 + n2 + n3) / 3;
+
+  //Saída
+  print('O valor do produto é: ${produto(10, 5)}');
+  print('O valor da divisão é: ${divisao(10, 2)}');
+  print('O valor do módulo é: ${modulo(5, 3)}');
+  print('O valor da média é: ${media(5, 5, 5)}');
+}
---
+++ Senac/Aula08_06-06-2022/funcoesComoParametrosDeFuncao.dart
@@ -0,0 +1,29 @@
+//As funções podem também receber parâmetros como funções
+import 'dart:math';
+
+void main() {
+  //Arrow function que simula cálculo de par ou ímpar
+  var par = () => print('Número Par');
+  var impar = () => print('Número ímpar');
+
+  print('-' * 50);
+  calcularParImpar(par, impar);
+  print('-' *50);
+}
+
+//Função
+void calcularParImpar(Function calcularPar, Function calcularImpar) {
+  //Criar um número aleatório
+  //O dart importa a biblioteca dart:math automaticamente
+  var numero = Random().nextInt(50);
+  print('Número randômico: $numero');
+
+  //Condicional para verificar se é par ou impar
+  //Verificando
+  if (numero % 2 == 0) {
+    calcularPar();
+  }
+  else {
+    calcularImpar();
+  }
+}
