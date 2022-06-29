ff274391f3a2e79455f58f96217c588d54086d12
nreiscaio <nreis.caio@gmail.com>
2022-06-28 21:43:54 -0300

    Aula Senac

---
+++ Senac/OrientacaoAObjetos/Aula14_28-06-2022/getEset.dart
@@ -0,0 +1,44 @@
+//get serve para pegar alguma coisa
+//set serve para configurar uma coisa
+
+class Conta {
+  //Atribuições
+  //o (_) underline significa privado (VISIBILIDADE)
+  //Não pode ser aceesado diretamente pelo meu objeto conta
+  //ou seja, só poderá ser utilizado dentro da classe
+  //1º PILAR DA OO: ENCAPSULAMENTO
+  double _saque; //privado
+  double saldo;
+
+  Conta([this._saque = 100, this.saldo = 1000.0]); //Construtor
+
+  //Como o atributo não pode ser acessado diretamente
+  //Vamos usar get e set para configurá-lo
+  //Fazendo isso não acessamos diretamnte nossos atributos
+  //Atenção: Só coloque essas configurações se realmente precisar
+  //Proteger seu atributo
+
+  //get - pegando o valor
+  //get não possui parâmetro e retorna valor
+  double get getSaque {
+    return this._saque;
+    //return 400.0;
+  }
+
+  //set - configurando o valor
+  //set possui parâmetro, mas não retorna valor
+
+  set setSaque(double saque) {
+    //Posso fazer aqui verificações e validações
+    if (saque > 0 && saque <= saldo) {
+      this._saque = saque;
+    }
+    else { 
+      print('Valor indisponível!');
+    }
+  }
+}
+
+void main() {
+  //Crian
+}
